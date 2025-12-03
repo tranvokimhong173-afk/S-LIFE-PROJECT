c37745aa5d00a59450d6ea99cbbdb033d548103c
tranvokimhong173-afk <tranvokimhong173@gmail.com>
2025-12-03 11:31:27 +0700

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,5 @@ const aiRoutes = require('./src/routes/ai.routes');
 app.use(express.json());
 
 app.use('/api/ai', aiRoutes);
-app.use('/api/email', emailRoutes);
-app.use('/api/utils', utilsRoutes);
 
 app.listen(3000, () => console.log("Server running on port 3000"));
